be7602bac42a1ac7fae7ac090b7d0be8a5934c27
Bobby De Luna <bobby.deluna2@gmail.com>
2023-01-04 23:56:18 -0700

    commit tree 82f8411af4ddb89e1f3cc625a5a525cea5e7ac5a

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,23 @@
 class Movie < ActiveRecord::Base
 
+def self.create_with_title (title)
+    Movie.create(title: title)
+end
+
+def self.first_movie
+    Movie.first
+end
+
+def self.last_movie
+    Movie.last
+end
+
+def self.movie_count
+    Movie.count
+end
+
+def find_movie_with_id (id)
+    Movie.where(id: id)
+end
+
 end
